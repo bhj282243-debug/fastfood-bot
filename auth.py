c5daef2506c98f943faf9910857ecfa8b75085ab
bhj282243-debug <bhj282243@gmail.com>
2026-07-01 23:43:43 +0500

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timedelta
 from typing import Optional
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError, jwt
 from passlib.context import CryptContext
@@ -8,7 +8,6 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 from database import get_db
 from models import User
-import schemas
 import os
 
 # Настройки безопасности
@@ -19,8 +18,6 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="api/login")
 
-router = APIRouter()
-
 # Функции безопасности
 def verify_password(plain_password, hashed_password):
     return pwd_context.verify(plain_password, hashed_password)
@@ -30,16 +27,6 @@ def create_access_token(data: dict):
     expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-
-async def authenticate_user(db: AsyncSession, username: str, password: str):
-    result = await db.execute(select(User).filter(User.username == username))
-    user = result.scalar_one_or_none()
-    
-    if not user:
-        return None
-    if not verify_password(password, user.hashed_password):
-        return None
-    return user
 
 async def get_current_admin(
     token: str = Depends(oauth2_scheme), 
@@ -64,16 +51,3 @@ async def get_current_admin(
     if user is None:
         raise credentials_exception
     return user
-
-# Маршрут для входа
-@router.post("/login", response_model=schemas.Token)
-async def login(login_data: schemas.LoginRequest, db: AsyncSession = Depends(get_db)):
-    user = await authenticate_user(db, login_data.username, login_data.password)
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect username or password",
-        )
-    
-    access_token = create_access_token(data={"sub": user.username})
-    return {"access_token": access_token, "token_type": "bearer"}
